3b2aabedded4bbe5951d3bfaa056cbfd33867d5a
Evodiatembi <evodytembi@gmail.com>
2024-02-27 03:29:14 -0800

    binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -46,5 +46,6 @@ void binary_tree_print(const binary_tree_t *);
 /* Task function prototypes */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 #endif /* BINARY_TREES_H */
