030444cc43a5be693632f483e8096d4b5af2feb8
Nekgate <Nekgate@gmail.com>
2024-02-27 13:40:54 +0100

    A function that creates a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,14 +12,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *newnode;
 
-    new = malloc(sizeof(binary_tree_t));
+    newnode = malloc(sizeof(binary_tree_t));
     if (newnode == NULL)
     {
             return (NULL); /* Mamory failed*/
     }
+    
     newnode->parent = parent;
     newnode->n = value;
     newnode->left = NULL;
     newnode->right = NULL;
-    return (new);
+    return (newnode);
 }
