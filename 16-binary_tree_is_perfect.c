3235ecb2ef208bb03be1db24a08697cf15663558
Nekgate <Nekgate@gmail.com>
2024-02-29 21:22:10 +0100

    A function that checks if a binary tree is complete

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -39,7 +39,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  *
  * Return: 1 if perfect, If tree is NULL return 0.
  */
-int binary_tree_is_perfect(const binary_tree_t *tree)
+
 {
 	binary_tree_t *lt, *rt;
 
