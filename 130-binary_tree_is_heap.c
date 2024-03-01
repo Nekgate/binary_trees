b33f2865d03be0a9573bcfdd81ff50e8ba2d69ad
Evodiatembi <evodytembi@gmail.com>
2024-03-01 14:06:54 -0800

    130-binary_tree_is_heap.c

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -7,27 +7,27 @@
  **/
 int check_max(const binary_tree_t *tree)
 {
-        int tempt1 = 1, tempt2 = 1;
+	int tempt1, tempt2;
 
-        if (tree != NULL)
-                return (0);
-        if (tree->left || tree->right)
-                return (0);
-        else
-                return (1);
-        if (tree->left)
-        {
-                if (tree->n <= tree->left->n)
-                        return (0);
-                tempt1 = check_max(tree->left);
-        }
-        if (tree->right)
-        {
-                if (tree->n <= tree->right->n)
-                        return (0);
-                tempt2 = check_max(tree->right);
-        }
-        return (tempt1 || tempt2);
+	if (tree != NULL)
+		return (0);
+	if (tree->left || tree->right)
+		return (0);
+	else
+		return (1);
+	if (tree->left)
+	{
+		if (tree->n <= tree->left->n)
+			return (0);
+		tempt1 = check_max(tree->left);
+	}
+	if (tree->left)
+	{
+		if (tree->n <= tree->right->n)
+			return (0);
+		tempt2 = check_max(tree->right);
+	}
+	return (tempt1 || tempt2);
 }
 /**
  * binary_tree_is_heap -function to checks if a valid max binary heap
