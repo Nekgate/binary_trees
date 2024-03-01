05d860759454e2be4ab3db82a0c8383ba7bb3983
Nekgate <Nekgate@gmail.com>
2024-03-01 20:39:42 +0100

    A function that inserts a value in an AVL Tree

---
+++ 121-avl_insert.c
@@ -0,0 +1,112 @@
+#include "binary_trees.h"
+
+/**
+ * check_balance - This checks the balance of each node
+ *
+ * @node: The pointer to the node
+ * @value: Input value
+ * Return: no return
+ */
+void check_balance(avl_t **node, int value)
+
+{
+	int balance;
+
+	balance = binary_tree_balance(*node);
+
+	if (balance > 1 && value < (*node)->left->n)
+	{
+		*node = binary_tree_rotate_right(*node);
+		return;
+	}
+
+	if (balance < -1 && value > (*node)->right->n)
+	{
+		*node = binary_tree_rotate_left(*node);
+		return;
+	}
+
+	if (balance > 1 && value > (*node)->left->n)
+	{
+		(*node)->left = binary_tree_rotate_left((*node)->left);
+		*node = binary_tree_rotate_right(*node);
+		return;
+	}
+
+	if (balance < -1 && value < (*node)->right->n)
+	{
+		(*node)->right = binary_tree_rotate_right((*node)->right);
+		*node = binary_tree_rotate_left(*node);
+		return;
+	}
+}
+
+/**
+ * avl_in - This checks if node is inserted
+ *
+ * @tree: The tree root
+ * @value: Node value
+ * Return: The pointer to the new node
+ */
+
+avl_t *avl_in(avl_t **tree, int value)
+{
+	avl_t *node;
+
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			return ((*tree)->left);
+		}
+		else
+		{
+			node = avl_in(&((*tree)->left), value);
+			if (node)
+				check_balance(tree, value);
+			return (node);
+		}
+	}
+
+	if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+		{
+			(*tree)->right = binary_tree_node(*tree, value);
+			return ((*tree)->right);
+		}
+		else
+		{
+			node = avl_in(&((*tree)->right), value);
+			if (node)
+				check_balance(tree, value);
+			return (node);
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+ * avl_insert - This inserts a value in a AVL Tree
+ *
+ * @tree: The tree root
+ * @value: Node value
+ * Return: The pointer to the new node
+ */
+
+avl_t *avl_insert(avl_t **tree, int value)
+{
+	avl_t *node;
+
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+
+	node = avl_in(tree, value);
+
+	return (node);
+}
